d4fd6fff44a6f36ce4474b4aa8223ff84a030880
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-06-30 08:53:28 -0400

    Make reset password form also dark

--- resources/views/livewire/pages/auth/reset-password.blade.php
+++ resources/views/livewire/pages/auth/reset-password.blade.php
@@ -73,31 +73,52 @@ new #[Layout('layouts.guest')] class extends Component
     <form wire:submit="resetPassword">
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="email" :value="__('Email')" class="text-gray-300" />
+            <x-text-input
+                wire:model="email"
+                id="email"
+                class="block mt-1 w-full bg-gray-700 border-gray-600 text-white"
+                type="email"
+                name="email"
+                required
+                autofocus
+                autocomplete="username"
+            />
+            <x-input-error :messages="$errors->get('email')" class="mt-2 text-red-400" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-label for="password" :value="__('Password')" class="text-gray-300" />
+            <x-text-input
+                wire:model="password"
+                id="password"
+                class="block mt-1 w-full bg-gray-700 border-gray-600 text-white"
+                type="password"
+                name="password"
+                required
+                autocomplete="new-password"
+            />
+            <x-input-error :messages="$errors->get('password')" class="mt-2 text-red-400" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                          type="password"
-                          name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="text-gray-300" />
+            <x-text-input
+                wire:model="password_confirmation"
+                id="password_confirmation"
+                class="block mt-1 w-full bg-gray-700 border-gray-600 text-white"
+                type="password"
+                name="password_confirmation"
+                required
+                autocomplete="new-password"
+            />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-red-400" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button class="bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 text-white">
                 {{ __('Reset Password') }}
             </x-primary-button>
         </div>
