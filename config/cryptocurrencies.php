136aa33854dbd36709532325ab680c780a60493e
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-06-30 16:53:26 -0400

    add more cryptos for pepe

--- config/cryptocurrencies.php
+++ config/cryptocurrencies.php
@@ -4,18 +4,55 @@ return [
     'coingecko' => [
         'base' => [
             'currencies' => [
-                'crypto_ids' => ['usd-coin', 'ethereum', 'tether', 'dai', 'brett', 'toshi', 'cardano', 'litecoin'], // CoinGecko IDs
-                'fiat' => ['usd', 'eur', 'jpy', 'gbp', 'cny', 'cad', 'aud', 'chf', 'xcg'], // Major fiat + XCG
+                'crypto_ids' => [
+                    'usd-coin',
+                    'ethereum',
+                    'tether',
+                    'dai',
+                    'brett',
+                    'toshi',
+                    'cardano',
+                    'litecoin',
+
+                    // New Base L2 notable tokens
+                    'axelar',
+                    'balancer',
+                    'yearn-finance',
+                    'cyberconnect',
+                    'optimism',
+                    'chainlink',
+                    'ski-token',
+                    'pepe',
+                    'degen',
+                    'miggless',
+                    'keycat',
+                ],
+                'fiat' => [
+                    'usd', 'eur', 'jpy', 'gbp', 'cny', 'cad', 'aud', 'chf', 'xcg',
+                ],
             ],
             'crypto_map' => [
-                'usd-coin' => 'USDC',
-                'ethereum' => 'ETH',
-                'tether' => 'USDT',
-                'dai' => 'DAI',
-                'brett' => 'BRETT',
-                'toshi' => 'TOSHI',
-                'cardano' => 'cbADA', // Wrapped Cardano on Base
-                'litecoin' => 'cbLTC', // Wrapped Litecoin on Base
+                'usd-coin'       => 'USDC',
+                'ethereum'       => 'ETH',
+                'tether'         => 'USDT',
+                'dai'            => 'DAI',
+                'brett'          => 'BRETT',
+                'toshi'          => 'TOSHI',
+                'cardano'        => 'cbADA',
+                'litecoin'       => 'cbLTC',
+
+                // New Base L2 mappings
+                'axelar'         => 'AXL',
+                'balancer'       => 'BAL',
+                'yearn-finance'  => 'YFI',
+                'cyberconnect'   => 'CYBER',
+                'optimism'       => 'OP',
+                'chainlink'      => 'LINK',
+                'ski-token'      => 'SKI',
+                'pepe'           => 'PEPE',
+                'degen'          => 'DEGEN',
+                'miggless'       => 'MIGGLES',
+                'keycat'         => 'KEYCAT',
             ],
         ],
     ],
