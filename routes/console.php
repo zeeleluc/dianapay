a0b75cc7fc8aeec0abf32539b0cb66556e2e9139
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-09-13 16:15:34 -0400

    no message

--- routes/console.php
+++ routes/console.php
@@ -17,5 +17,5 @@ if (app()->environment('prod')) {
 // ========== Testing/High-Frequency Poll ==========
 if (app()->environment('prod')) {
     // Run every 30 seconds in testing mode
-    $schedule->command('poll-solana-tokens')->everyThirtySeconds();
+//    $schedule->command('poll-solana-tokens')->everyThirtySeconds();
 }
