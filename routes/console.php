c87cb73c3646b4dbd128d0e557e8e7f91dde73e1
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-09-14 15:40:53 -0400

    no message

--- routes/console.php
+++ routes/console.php
@@ -16,8 +16,8 @@ if (app()->environment('prod')) {
 
 // ========== Testing/High-Frequency Poll ==========
 if (app()->environment('prod')) {
-    $schedule->command('solana:poll-solana-tokens')->everyThirtySeconds();
-    $schedule->command('solana:buy-new-solana-tokens')->everyThirtySeconds();
-    $schedule->command('solana:auto-sell')->everyFiveSeconds();
-    $schedule->command('solana:clean-failed-calls')->everyMinute();
+//    $schedule->command('solana:poll-solana-tokens')->everyThirtySeconds();
+//    $schedule->command('solana:buy-new-solana-tokens')->everyThirtySeconds();
+//    $schedule->command('solana:auto-sell')->everyFiveSeconds();
+//    $schedule->command('solana:clean-failed-calls')->everyMinute();
 }
